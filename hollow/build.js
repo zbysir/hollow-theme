45061d00aca8017e32130b017ca54b05895756f1
zhangliang <zbysir@qq.com>
2022-12-28 15:55:41 +0800

    use esbuild-style-plugin

--- hollow/build.js
+++ hollow/build.js
@@ -1,7 +1,6 @@
 const esbuild = require("esbuild");
 const autoprefixer = require("autoprefixer");
 const tailwindcss = require('tailwindcss')
-const fs = require("fs-extra");
 const stylePlugin = require('esbuild-style-plugin')
 
 esbuild
@@ -11,31 +10,11 @@ esbuild
       "main.css",
       // 前端组件
       "app/index.tsx",
-      // index 是生成时的文件，只是用于收集依赖，当文件更改的时候 rebuild 生成 css（tailwindcss 需要从文件中收集依赖）
-      // index 不会打包出文件
-      "index.tsx"
     ],
     bundle: true,
     plugins: [
-      {
-        name: "remove",
-        setup: function (options) {
-          // 删不要的 index.tsx
-          options.onEnd(function (args) {
-            for (let outputsKey in args.metafile?.outputs) {
-              if (args.metafile.outputs[outputsKey].entryPoint === "index.tsx") {
-                fs.rm(outputsKey)
-                fs.rm(outputsKey + '.map')
-              }
-            }
-            return null
-          })
-        }
-      },
-
       stylePlugin({
         postcss: {plugins: [tailwindcss, autoprefixer]},
-        extract: true
       })
     ],
     external: ['@bysir/hollow'],
