fe8221f986cb031b7cf3259854f30ecd945c920a
zhangliang <zbysir@qq.com>
2023-01-10 16:45:14 +0800

    use 'new Date().getFullYear()' on right

--- compass/particle/Footer.tsx
+++ compass/particle/Footer.tsx
@@ -1,5 +1,3 @@
-import hollow from "@bysir/hollow"
-
 interface Props {
     name: string
     stack: string
@@ -40,6 +38,8 @@ const iconSvg = {
     </svg>
 }
 
+const year = new Date().getFullYear()
+
 export default function Footer(props: Props) {
     let thin = true
 
@@ -49,7 +49,7 @@ export default function Footer(props: Props) {
             <div class="container flex flex-col items-center py-8 mx-auto max-w-6xl sm:flex-row">
                 {/*<a href="#_" class="text-xl font-black leading-none text-gray-900 select-none logo">{props.name}<span*/}
                 {/*    class="text-indigo-600">.</span></a>*/}
-                <p class="mt-4 text-sm text-gray-500 sm:border-gray-200 sm:mt-0">© 2022 {props.name}'s Blog - <span
+                <p class="mt-4 text-sm text-gray-500 sm:border-gray-200 sm:mt-0">© {year} {props.name}'s Blog - <span
                     dangerouslySetInnerHTML={{__html: props.stack}}></span>
                 </p>
                 <span class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
