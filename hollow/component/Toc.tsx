244d4d0da4d04bedfbc8201685f5b4369bf560ff
zhangliang <zbysir@qq.com>
2023-03-28 14:43:00 +0800

    Fix Toc component

--- hollow/component/Toc.tsx
+++ hollow/component/Toc.tsx
@@ -1,13 +1,15 @@
-export const Toc = ({title = '', items = [], id = '', level = 0}) => {
+export const Toc = ({title = '', items = [], id = ''}) => {
     if (title) {
-        return <li >
+        return <li>
             <a href={"#" + id} className="t-p-1 t-block">{title}</a>
 
-            <ul style={{paddingLeft: level * 20 + 'px'}}>
-                {items?.map(i => <Toc {...i} level={level + 1}></Toc>)}
+            <ul style={{paddingLeft: 20 + 'px'}}>
+                {items?.map(i => <Toc {...i}></Toc>)}
             </ul>
         </li>
     } else {
-        return <ul style={{paddingLeft: level * 20 + 'px'}}> {items?.map(i => <Toc {...i} level={level + 1}></Toc>)}</ul>
+        return <ul style={{paddingLeft: 20 + 'px'}}>
+            {items?.map(i => <Toc {...i} ></Toc>)}
+        </ul>
     }
 }
